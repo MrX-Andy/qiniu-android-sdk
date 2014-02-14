e90b8b3d3453e54745203921d29704aa736a9b39
simon-liubin <simon.liubin@gmail.com>
2014-02-14 14:12:34 +0800

    httpClient 添加超时，修改生成方式

--- src/main/com/qiniu/client/util/HttpHelper.java
+++ src/main/com/qiniu/client/util/HttpHelper.java
@@ -14,36 +14,70 @@ import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
 
 import com.qiniu.client.auth.Authorizer;
 
-
 public class HttpHelper {
-    public static HttpClient getHttpClient() {
-        HttpParams httpParams = new BasicHttpParams();
-        ConnManagerParams.setMaxTotalConnections(httpParams, 10);
-        ConnPerRoute connPerRoute = new ConnPerRouteBean(3);
-        ConnManagerParams.setMaxConnectionsPerRoute(httpParams, connPerRoute);
-        HttpProtocolParams.setVersion(httpParams, HttpVersion.HTTP_1_1);
-        HttpConnectionParams.setConnectionTimeout(httpParams, 45000);
+	private static HttpClient httpClient;
 
-        SchemeRegistry registry = new SchemeRegistry();
-        registry.register(
-                new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-        registry.register(
-                new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
-        ClientConnectionManager cm = new ThreadSafeClientConnManager(httpParams, registry);
+	public static HttpClient getHttpClient() {
+		if (httpClient == null) {
+			httpClient = buildHttpClient();
+		}
+		return httpClient;
+	}
 
-        HttpClient httpClient = new DefaultHttpClient(cm, httpParams);
-        return httpClient;
-    }
+	public static void closeExpiredConnections() {
+		closeExpiredConnections(httpClient);
+	}
 
-    public static HttpPost buildUpPost(String url, Authorizer authorizer) {
-        HttpPost post = new HttpPost(url);
-        post.setHeader("Authorization", "UpToken " + authorizer.getUploadToken());
-        return post;
-    }
+	public static void closeExpiredConnections(HttpClient httpClient) {
+		if (httpClient != null) {
+			httpClient.getConnectionManager().closeExpiredConnections();
+		}
+	}
+
+	public static void shutdown() {
+		shutdown(httpClient);
+		httpClient = null;
+	}
+
+	public static void shutdown(HttpClient httpClient) {
+		if (httpClient != null) {
+			httpClient.getConnectionManager().shutdown();
+		}
+	}
+
+	private static HttpClient buildHttpClient() {
+		HttpParams httpParams = new BasicHttpParams();
+		ConnManagerParams.setMaxTotalConnections(httpParams, 10);
+		ConnPerRoute connPerRoute = new ConnPerRouteBean(3);
+		ConnManagerParams.setMaxConnectionsPerRoute(httpParams, connPerRoute);
+		HttpProtocolParams.setVersion(httpParams, HttpVersion.HTTP_1_1);
+
+		SchemeRegistry registry = new SchemeRegistry();
+		registry.register(new Scheme("http", PlainSocketFactory
+				.getSocketFactory(), 80));
+		registry.register(new Scheme("https", SSLSocketFactory
+				.getSocketFactory(), 443));
+		ClientConnectionManager cm = new ThreadSafeClientConnManager(
+				httpParams, registry);
+
+		HttpClient httpClient = new DefaultHttpClient(cm, httpParams);
+		
+		httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 1000 * 60);
+		httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 1000 * 60);
+		
+		return httpClient;
+	}
+
+	public static HttpPost buildUpPost(String url, Authorizer authorizer) {
+		HttpPost post = new HttpPost(url);
+		post.setHeader("Authorization",
+				"UpToken " + authorizer.getUploadToken());
+		return post;
+	}
 }
